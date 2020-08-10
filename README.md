112c05763955497fcd95c664ee9b971f6fb548ba
Mathi Gatti <mathigatti@gmail.com>
2020-08-10 11:33:17 -0300

    renaming and colab

--- README.md
+++ README.md
@@ -1,36 +1,12 @@
-# cogerbot
-GPT-2 Sexting bot
+# GPT-2 Chatbot
+This is a basic chatbot using GPT-2.
 
-sudo FLASK_APP=app.py FLASK_ENV=development flask run --port 80 --host 0.0.0.0
+## Usage
 
+You can check how to use it and try it on colab.
 
-## Data
-- real sexting conversations
-
-	https://thoughtcatalog.com/eko-hayden/2016/04/erotic-sexts-between-lovers-that-will-make-you-horny/
-
-	https://www.wattpad.com/31257965-erotic-kik-conversations
-
-	https://www.manwhore.org/real-sexting-conversation-to-read/
-
-	https://ar.pinterest.com/pin/568790627910938307/
-
-	https://www.cosmopolitan.com/sex-love/news/a52121/sexting-messages-tips/
-
-	https://www.talktome.com/news/real-sexting-conversations/
-
-	https://imgur.com/a/Kqdiu
-
-## Similar products
-- juicebox
-- bottr.me
 
 ## Ideas for later
 
-- Send images
-	Fotos porno -> face depixelizer -> nude con cara inventada
-- Hotness knob
-- Penis/Pussy toggle
-- Fixes vocabulario español Argentino
-	- concha -> coño
-	- coger -> follar
+- Send images and gifs
+- Personality and background parameters
--- chatbot.py
+++ chatbot.py
@@ -1,13 +1,10 @@
 from aitextgen import aitextgen
 
-human = "He: "
-bot = "She: "
-full_chat = f"{human}I want to fuck you hottie\n{bot}Please fuck me really hard\n{human}Your pussy is really wet\n{bot}My pussy is tight and can take a lot of pain\n"
-#full_chat = f"{human}My lord, how can I stop this pain?.\n{bot}This will stop the day the truthful will benefit from their truthfulness.\n{human} Thanks my lord, for them are gardens in Paradise to live forever.\n{bot}Those who follower me will be rewarded, my son.\n"
+human = "The man said, "
+bot = "God said, "
+full_chat = f"{human}Is heaven a good place?\n{bot}The Bible says so.\n{human}Then it must be true my lord.\n{bot}Yes, these are my words son.\n"
 
 def start_model():
-	# Para descargar el modelo la primera vez ejecutar la siguiente linea
-	#ai = aitextgen(tf_gpt2="355M", to_gpu=True)
 	ai = aitextgen(model="aitextgen/pytorch_model_355M.bin", config="aitextgen/config_355M.json", to_gpu=True)
 	return ai
 
@@ -17,9 +14,12 @@ def answer(previous_chat):
 	previous_chat = "\n".join(previous_chat.split("\n")[-12:]) + "\n"
 	previous_chat = previous_chat + bot
 
-	answer = ai.generate_one(prompt=previous_chat, max_length=len(previous_chat) + 50)
+	answer = ai.generate_one(prompt=previous_chat, max_length=50)
 	answer = answer[len(previous_chat):].split("\n")[0]
 	if len(answer.split(".")) > 1:
 		answer = ".".join(answer.split(".")[:-1])
+
+	if len(answer) < 2:
+		return "*NO ANSWER*"
 
 	return answer
